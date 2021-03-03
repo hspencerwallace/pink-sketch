06188c327efa7b42db10bcfcad283d6138645a14
Helen Spencer-Wallace <Caverna@Helen-Spencer-Wallaces-MacBook-Pro-1358.local>
2021-03-02 15:54:32 -1000

    zoom rectangle in webgl

--- sketch.js
+++ sketch.js
@@ -1,27 +1,18 @@
-let x, y;
+let angle = 0;
 
 function setup() {
-  createCanvas(720, 400);
-  // Starts in the middle
-  x = width / 2;
-  y = height;
+  createCanvas(800, 800, WEBGL);
+ 
 }
 
 function draw() {
-  background(200);
-  
-  // Draw a circle
-  stroke(50);
-  fill(100);
-  ellipse(x, y, 24, 24);
-  
-  // Jiggling randomly on the horizontal axis
-  x = x + random(-1, 1);
-  // Moving up at a constant speed
-  y = y - 1;
-  
-  // Reset to the bottom
-  if (y < 0) {
-    y = height;
-  }
+  background(255);
+  rectMode(CENTER);
+  fill(0,0,0);
+  // rotateX(angle);
+  rect(0, -350, 800, 100);
+	rect(0, 300, 800, 300);
+	rect(350, 0, 100, 800);
+	rect(-350, 0, 100, 800);
+  angle += 0.025;
 }
