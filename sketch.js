a0bc2b2ed54f57f17a642051d68454cb9886780c
Helen Spencer-Wallace <Caverna@Helen-Spencer-Wallaces-MacBook-Pro-1358.local>
2021-03-02 15:18:17 -1000

    testing_with_floating_ball

--- sketch.js
+++ sketch.js
@@ -1,12 +1,27 @@
-function setup() {
-  // put setup code here
-  createCanvas(800, 800);
+let x, y;
 
+function setup() {
+  createCanvas(720, 400);
+  // Starts in the middle
+  x = width / 2;
+  y = height;
 }
 
 function draw() {
-  // put drawing code here
-
-  ellipse(mouseX, mouseY, 600, 100);
-
+  background(200);
+  
+  // Draw a circle
+  stroke(50);
+  fill(100);
+  ellipse(x, y, 24, 24);
+  
+  // Jiggling randomly on the horizontal axis
+  x = x + random(-1, 1);
+  // Moving up at a constant speed
+  y = y - 1;
+  
+  // Reset to the bottom
+  if (y < 0) {
+    y = height;
+  }
 }
